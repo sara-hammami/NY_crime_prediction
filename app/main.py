0c0acbe1853290d7615e940cda5b8a5147801a26
sarra <sarra.hammami456@gmail.com>
2023-12-28 18:11:10 +0100

    loaded the trained_model

---
+++ app/main.py
@@ -0,0 +1,124 @@
+import streamlit as st
+import folium
+import pandas as pd
+from streamlit_folium import folium_static
+from datetime import datetime
+import clipboard
+from branca.element import Template, MacroElement
+import process as pr
+
+class ClickForLatLng(MacroElement):
+    _template = Template(u"""
+                {% macro script(this, kwargs) %}
+                    var marker = new Array();
+                    function getLatLng(e){  
+                        if (marker.length >0) {
+                            for(i=0;i<marker.length;i++){
+                                {{this._parent.get_name()}}.removeLayer(marker[i])
+                            }
+                        }
+                        var new_mark = L.marker().setLatLng(e.latlng).addTo({{this._parent.get_name()}});
+                        marker.push(new_mark);
+                        new_mark.dragging.enable();
+                        new_mark.on('dblclick', function(e){ {{this._parent.get_name()}}.removeLayer(e.target)})
+                        var lat = e.latlng.lat.toFixed(6),
+                        lng = e.latlng.lng.toFixed(6);
+                        var txt = {{this.format_str}};
+                        navigator.clipboard.writeText(txt);
+                        new_mark.bindPopup({{ this.popup }});
+                        };
+                    {{this._parent.get_name()}}.on('click', getLatLng);
+                    
+                {% endmacro %}
+                """)
+
+    def __init__(self,popup=None):
+        super(ClickForLatLng, self).__init__()
+        self._name = 'ClickForLatLng'
+        self.format_str = 'lat + "," + lng'
+        self.alert = True
+        self.lat_long = clipboard.paste().split(',')
+        if popup:
+            self.popup = ''.join(['"', popup, '"'])
+        else:
+            self.popup = '"Latitude: " + lat + "<br>Longitude: " + lng '
+
+
+def generateBaseMap(default_location=[40.704467, -73.892246], default_zoom_start=11,min_zoom=11,max_zoom=15,):
+    base_map = folium.Map(location=default_location, control_scale=True, zoom_start=default_zoom_start, min_zoom=min_zoom, max_zoom=max_zoom,max_bounds=True, min_lat= 40.47739894 , min_lon= -74.25909008, max_lat= 40.91617849,max_lon=-73.70018092)
+    return base_map
+
+
+def init():
+    with st.sidebar:
+        st.header("Enter your information", )
+        gender = st.radio("Gender:", ["Male","Female"],key="vic1")
+        race = st.selectbox("Race:",['WHITE', 'WHITE HISPANIC', 'BLACK', 'ASIAN / PACIFIC ISLANDER', 'BLACK HISPANIC', 'AMERICAN INDIAN/ALASKAN NATIVE', 'OTHER'],key="vic2")
+        age = st.slider("Age:",0,120,key="vic3")
+        date = st.date_input("Date:",datetime.now())
+        hour = st.slider("Hour:",min_value=0,max_value=24)
+        place = st.radio("Place:",("In park","In public housing","In station"))
+        _,col,_ = st.sidebar.columns(3)   
+        with  col:
+            predict = st.button("predict")
+        
+    return gender, race, age, predict, date, hour, place
+
+st.set_page_config(
+    page_title="NYC Crime Prediction",
+    page_icon="🌍",  # You can use any emoji or a URL to an image
+    layout="wide",  # or "centered" or "wide"
+    initial_sidebar_state="expanded",  # or "collapsed"
+)
+
+# Custom CSS style for the red box
+red_box_style = """
+    <style>
+        .red-box {
+            background-color:tomato;
+            padding: 10px;
+            border-radius: 5px;
+            margin-bottom:5px;
+            color: white;
+            text-align: center;
+        }
+    </style>
+"""
+
+# Display the custom style
+st.markdown(red_box_style, unsafe_allow_html=True)
+
+# Create the red box around the title
+st.markdown('<div class="red-box"><h1>New York Crime Prediction</h1></div>', unsafe_allow_html=True)
+gender, race, age, predict, date, hour, place= init()
+base_map = generateBaseMap()
+click = ClickForLatLng()
+base_map.add_child(click)
+
+lat_long = click.lat_long
+if len(lat_long) == 2:
+    lat = lat_long[0]
+    long = lat_long[1]
+
+else :
+    lat = ""
+    long = ""
+# try:
+    # print(float(lat),float(long))
+    # folium.Marker([float(lat),float(long)]).add_to(base_map)
+# except:
+#     print("no marker")
+
+x = folium_static(base_map)
+
+if predict:
+    if lat=='' or long == '':
+        st.error("Please make sure that you selected a location on the map")    
+        if st.button("Okay"):
+            pass
+    else:
+        X = pr.create_df(hour,date.month,date.day,lat,long,place,age,race,gender)
+        pred, crimes = pr.predict(X)
+        st.markdown(f"You are likely to be a victim of: **{pred}**")
+        st.markdown(f"#### Some of the crimes types are the following: ")
+        st.markdown(crimes)
---
+++ app/process.py
@@ -0,0 +1,64 @@
+from numpy.lib.shape_base import expand_dims
+import pandas as pd
+import requests
+import numpy as np
+import joblib
+
+api_endpoint = 'https://locatenyc.io/arcgis/rest/services/locateNYC/v1/GeocodeServer/reverseGeocode?location='
+api_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InNhcmE3IiwiZXhwaXJlcyI6MTcwNjM2ODM5MTI5MSwicGVybWlzc2lvbnMiOiJiYXNpYyIsImlhdCI6MTcwMzc3NjM5MSwiZXhwIjoxNzA2MzY4MzkxfQ.YsENiyFi9pEulme0S22oUimOqHu-MqdnrBeDHsQBwXk"
+model = joblib.load(r"D:/AIM-tbourbi/projet/app/saved_modals/xgboost.joblib")
+crime_classes = {0:'Violation',1:"Misdemeanor",2:"Felony"}
+felony = open(r"D:/AIM-tbourbi/projet/app/crime_classes/felony.txt","r").read()
+mis = open(r"D:/AIM-tbourbi/projet/app/crime_classes/misdemeanor.txt","r").read()
+violation = open(r"D:/AIM-tbourbi/projet/app/crime_classes/violation.txt","r").read()
+
+def create_df(hour,month,day,latitude,longitude,place,vic_age,vic_race,vic_sex):
+
+
+    hour = int(hour) if int(hour) < 24 else 0
+    api_data = None
+    pct = 0  # Initialize pct to a default value
+    try:
+        api_data = requests.get(f'{api_endpoint}{longitude},{latitude}&distance=1000&token={api_token}').json()['address']
+        pct, boro = int(api_data["policePrecinct"]), api_data["Borough"]
+        boro = boro.upper()
+    except Exception as e:
+        print(f"Error during API call: {e}")
+
+    month = int(month)
+    day = int(day)
+    in_park = 1 if place == "In park" else 0
+    in_public = 1 if place == "In public housing" else 0
+    in_station = 1 if place == "In station" else 0
+
+
+    columns = np.array(['EVENT_TIME', 'ADDR_PCT_CD', 'month', 'day', 'Latitude',
+       'Longitude', 'IN_PARK', 'IN_PUBLIC_HOUSING', 'IN_STATION','BORO_NM_BRONX','BORO_NM_BROOKLYN', 'BORO_NM_MANHATTAN',
+       'BORO_NM_QUEENS', 'BORO_NM_STATEN ISLAND', 'BORO_NM_UNKNOWN',
+       'VIC_AGE_GROUP_18-24','VIC_AGE_GROUP_25-44', 'VIC_AGE_GROUP_45-64', 'VIC_AGE_GROUP_65+',
+       'VIC_AGE_GROUP_<18', 'VIC_AGE_GROUP_UNKNOWN',
+       'VIC_RACE_AMERICAN INDIAN/ALASKAN NATIVE','VIC_RACE_ASIAN / PACIFIC ISLANDER', 'VIC_RACE_BLACK',
+       'VIC_RACE_BLACK HISPANIC', 'VIC_RACE_OTHER', 'VIC_RACE_UNKNOWN',
+       'VIC_RACE_WHITE', 'VIC_RACE_WHITE HISPANIC', 'VIC_SEX_D', 'VIC_SEX_E',
+       'VIC_SEX_F', 'VIC_SEX_M', 'VIC_SEX_U'])
+
+    data = [[hour,114 if api_data == None else pct,month,day,latitude,longitude,in_park,in_public,
+       in_station,0 if api_data == None else 1 if boro == "BRONX" else 0, 0 if api_data == None else 1 if boro == "BROOKLYN" else 0,0 if api_data == None else 1 if boro == "MANHATTAN" else 0,0 if api_data == None else 1 if boro == "QUEENS" else 0,
+       0 if api_data == None else 1 if boro == "STATEN ISLAND" else 0,1 if api_data == None else 1 if boro not in("BRONX","BROOKLYN","MANHATTAN","QUEENS", "STATEN ISLAND") else 0,
+       1 if vic_age in range(18,25) else 0, 1 if vic_age in range(25,45) else 0, 1 if vic_age in range(45,65) else 0, 1 if vic_age>=65 else 0,
+       1 if vic_age < 18 else 0, 0, 1 if vic_race == "AMERICAN INDIAN/ALASKAN NATIVE" else 0, 1 if vic_race == "ASIAN / PACIFIC ISLANDER" else 0,
+       1 if vic_race == "BLACK" else 0, 1 if vic_race == "BLACK HISPANIC" else 0, 1 if vic_race == "OTHER" else 0,
+       1 if vic_race == "UNKNOWN" else 0, 1 if vic_race == "WHITE" else 0, 1 if vic_race == "WHITE HISPANIC" else 0,
+       0,0,1 if vic_sex == "Female" else 0,1 if vic_sex == "Male" else 0, 0]]
+
+    df = pd.DataFrame(data,columns=columns)
+    return df.values
+
+def predict(data):
+   pred = model.predict(data)[0]
+   if (pred == 0):
+      return crime_classes[pred], violation
+   elif pred==1:
+      return crime_classes[pred], mis
+   else:
+      return crime_classes[pred], felony
